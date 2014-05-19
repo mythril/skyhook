72a67ab011306a880c1473d3e7c1e2aeda831fe9
Lucas Green <lucas.m.green@gmail.com>
2014-05-19 05:42:39 -0700

    Added: FiatMeta ARS implementation

---
+++ includes/Currencies/ARS.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Currencies;
+use Amount;
+
+class AUD implements FiatMeta {
+	use Common;
+	
+	public function getDenominations() {
+		return [
+			1 => new Amount('2'),
+			2 => new Amount('5'),
+			3 => new Amount('10'),
+			4 => new Amount('20'),
+			5 => new Amount('50'),
+			6 => new Amount('100'),
+		];
+	}
+}
+
