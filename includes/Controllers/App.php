5d0e185a9fd13348044cccc2e42741d5ee749ccd
Lucas Green <lucas.m.green@gmail.com>
2014-07-11 23:08:04 +0200

    Added: Content-Security-Policy headers to restrict externally sourced code from this page.

--- includes/Controllers/App.php
+++ includes/Controllers/App.php
@@ -9,7 +9,14 @@ class App implements Controller {
 	public function execute(array $matches, $url, $rest) {
 		$admin = AdminConfig::volatileLoad();
 		$contactInfo = $admin->getConfig()->getContactInformation();
-		
+		header("Content-Security-Policy: ". 
+			implode("; ", [
+				"default-src 'self'",
+				"script-src 'self' 'unsafe-eval'",
+				"object-src 'none'",
+				"report-uri /report;",
+			])
+		);
 		$tmpl = new Template('app');
 		$tmpl->render([]);
 		return true;
